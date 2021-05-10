808d76d58617276df492295e881bbe273db33c0f
denefc <mehmet_7kezman@hotmail.com>
2021-05-10 20:54:15 +0300

    half done w/newdb

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.entities.concretes;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employers")
+public class Employer {
+
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "verify_system")
+    private boolean verify;
+}
---
+++ src/main/java/kodlamaio/hrms/entities/concretes/User.java
@@ -0,0 +1,28 @@
+package kodlamaio.hrms.entities.concretes;
+
+
+import lombok.Data;
+
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public  class User  {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "mail")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "verify")
+    private boolean verify;
+
+}
